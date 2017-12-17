5307975a4b2f4cbd2e4ec0d8fa67699ffc3e1df8
Boris <fedotom2@gmail.com>
2017-12-17 21:40:39 +0200

    Lesson 3, task 5

--- lesson3/task5.js
+++ lesson3/task5.js
@@ -1,8 +1,7 @@
 'use strict';
 
-// TASK 5 NOT WORKING!
-// const genArr = () => 
-//   new Array(Math.round(Math.random() * 50))
-//     .map((el) => Math.round(Math.random() * 50));
+const genArr = () => 
+  [...new Array(Math.round(Math.random() * 50))]
+    .map((el) => Math.round(Math.random() * 50));
 
-// console.log(genArr());
+console.log(genArr());
